e48f4b6583fabc25fe7562214252a4f941077f78
Your Name <you@example.com>
2022-03-25 16:50:07 +0600

    home page done

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,11 +1,60 @@
-import React from 'react';
-import "./Login.css"
+import React from "react";
+import { FloatingLabel, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import "./Login.css";
+
 const Login = () => {
-    return (
-      <div className="d-flex justify-content-center">
-        <div className="display-1 text-warning text-center login">Login here</div>
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example"));
+
+  return (
+    <div className="d-flex justify-content-center mt-5 log-section">
+      <div className="login text-light px-4">
+        <h3 className="fw-bold py-3 pt-4 text-center">Login</h3>
+
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label for="email" className="mb-2 fs-4">
+            Email:
+          </label>{" "}
+          <br />
+          <input
+            className="input-field"
+            type={"email"}
+            {...register("example")}
+          />{" "}
+          <br />
+          <label htmlFor="Email" className="my-2 mt-4 fs-4">
+            Password:
+          </label>{" "}
+          <br />
+          <input
+            className="input-field"
+            {...register("exampleRequired", { required: true })}
+          />{" "}
+          <br />
+          {errors.exampleRequired && <span>This field is required</span>} <br />
+          <input
+            type="submit"
+            className="submit-btn d-flex justify-content-center mx-auto"
+          />
+          <hr />
+          <p className="text-center login-with">Or</p>
+        </form>
+        <button className="google-btn">
+          {" "}
+          <img src="https://i.ibb.co/HPd5k52/pngwing-com.png" alt="" width='25px' /> Login
+          with Google
+        </button>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
