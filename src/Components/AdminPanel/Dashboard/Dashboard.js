696a59e4fcd8f49709b9fe59619db4060cc0a0ad
Your Name <you@example.com>
2022-03-27 16:04:57 +0600

    dashboard design complete

--- src/Components/AdminPanel/Dashboard/Dashboard.js
+++ src/Components/AdminPanel/Dashboard/Dashboard.js
@@ -18,12 +18,25 @@ const Dashboard = () => {
       <div className="dash-wrapper2">
         <h1>This is Dashboard</h1>
         <Container>
-          <Row className="d-flex justify-content-around">
+          <Row className="d-flex justify-content-around pt-5">
             <Col md={4}>
-              <h1>Total students {student.length}</h1>
+              <div className="total-student  p-5 rounded">
+                <h3 className="text-center">Total students</h3>
+                <h4 className="text-center">{student.length}</h4>
+              </div>
             </Col>
-            <Col md={4}>iuiu</Col>
-            <Col md={4}>iuifi</Col>
+            <Col md={4}>
+              <div className="total-student  p-5 rounded">
+                <h3 className="text-center">Present students</h3>
+                <h4 className="text-center">75</h4>
+              </div>
+            </Col>
+            <Col md={4}>
+              <div className="total-student  p-5 rounded">
+                <h3 className="text-center">Take Food </h3>
+                <h4 className="text-center">41</h4>
+              </div>
+            </Col>
           </Row>
         </Container>
       </div>
--- src/Components/AdminPanel/MakeStudents/MakeStudent.js
+++ src/Components/AdminPanel/MakeStudents/MakeStudent.js
@@ -1,7 +1,64 @@
 import React from "react";
-
+import { Container } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import "./MakeStudent.css"
 const MakeStudent = () => {
-  return <div>Make your students</div>;
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className="home-bg">
+      <h1 className="text-decoration-underline text-center text-light py-3 ">
+        New Student
+      </h1>
+
+      <Container className="text-light ">
+        <div className="admission-form-wrapper ms-5 mt-5 px-5  pt-3 pb-5 ">
+          <h3 className="text-center">Register</h3>
+          <form onSubmit={handleSubmit(onSubmit)} className="admission-form">
+            <input
+              placeholder="ID"
+              {...register("id", { required: true })}
+              className=""
+            />
+            {errors.idRequired && <span>This field is required</span>}
+            <input
+              placeholder="Name"
+              {...register("name", { required: true })}
+              className=""
+            />
+            {errors.nameRequired && <span>This field is required</span>}
+            <br />
+            <input
+              placeholder="Email"
+              {...register("email", { required: true })}
+            />{" "}
+            {errors.emailRequired && <span>This field is required</span>}
+            <input
+              placeholder="Class/Department"
+              {...register("class", { required: true })}
+              className=""
+            />
+            {errors.classRequired && <span>This field is required</span>}
+            <br />
+            <input
+              placeholder="Section"
+              {...register("section", { required: true })}
+              className=""
+            />
+            {errors.sectionRequired && <span>This field is required</span>}
+            <br />
+            <input type="submit" className="submit-btn" />
+          </form>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default MakeStudent;
