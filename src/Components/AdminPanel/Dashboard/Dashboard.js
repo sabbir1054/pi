3a32cc93f9d940324664ee5d47aef51f98506986
Your Name <you@example.com>
2022-03-27 08:16:24 +0600

    json data added

--- src/Components/AdminPanel/Dashboard/Dashboard.js
+++ src/Components/AdminPanel/Dashboard/Dashboard.js
@@ -1,11 +1,30 @@
-import React from 'react';
-
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "./Dashboard.css";
 const Dashboard = () => {
-    return (
-        <div>
-            <h1>This is Dashboard</h1>
-        </div>
-    );
+  const [student, setStudent] = useState();
+
+  // load student data
+  useEffect(() => {
+    fetch("")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return (
+    <div className="dash-wrapper">
+      <div className="dash-wrapper2">
+        <h1>This is Dashboard</h1>
+        <Container>
+          <Row className="d-flex justify-content-around">
+            <Col md={4}>uyiyu</Col>
+            <Col md={4}>iuiu</Col>
+            <Col md={4}>iuifi</Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
